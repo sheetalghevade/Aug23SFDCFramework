2464477444161368406e4e0477290b43e113b400
sheetalghevade <ghevade.sheetal@gmail.com>
2023-08-23 17:47:58 -0700

    6th Commit

--- src/main/java/testcases/test.java
+++ src/main/java/testcases/test.java
@@ -23,7 +23,7 @@ public class test {
 		pswrd.clear();
 		pswrd.sendKeys("test1234");
 		
-		driver.findElement(By.id("Login")).click();
+//		driver.findElement(By.id("Login")).click();
 		
 		Thread.sleep(4000);
 		
