4cdb4953bcbc9e5fd8706e609b17a4955af4ac75
sheetalghevade <ghevade.sheetal@gmail.com>
2023-08-25 23:51:21 -0700

    removed target class file

--- src/main/java/testcases/test.java
+++ src/main/java/testcases/test.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class test {
      static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-        WebDriverManager.firefoxdriver().setup();
+       
 		
 		driver = new FirefoxDriver();
 		driver.get("https://login.salesforce.com");
